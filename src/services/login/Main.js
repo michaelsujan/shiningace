a9430d355c1b29534b60eb2dfe1cf54c140f8114
michaelsujan <kmichaelsujan@gmail.com>
2020-08-23 17:42:42 +0530

    Login Page

---
+++ src/services/login/Main.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mysql = require("mysql");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+const Router = require("./Router");
+
+app.use(express.static(path.join(__dirname, "build")));
+app.use(express.json());
+
+//Database
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "thisisawesome7",
+  database: "myapp",
+});
+
+db.connect(function (err) {
+  if (err) {
+    console.log("DB error");
+    throw err;
+    return false;
+  }
+});
+
+const sessionStore = new MySQLStore(
+  {
+    expiration: 1825 * 86400 * 1000,
+    endConnectionOnClose: false,
+  },
+  db
+);
+
+app.use(
+  session({
+    ke: "asfvjsdjbnadfvnlkasndfv",
+    secret: "ajbviuabbsDVUIASLIUVB",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1825 * 86400 * 1000,
+      httpOnly: false,
+    },
+  })
+);
+
+new Router(app, db);
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(3000);
